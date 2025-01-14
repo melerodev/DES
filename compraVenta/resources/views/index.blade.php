546d85589f02eb0b5179f2b22b50caa51624a672
Alejandro Melero Zhohal <brosgenepic@gmail.com>
2025-01-14 09:39:23 +0000

    Mejorada la visualización de productos en la página de inicio y actualizado el enlace para crear un nuevo producto a un botón

--- compraVenta/resources/views/index.blade.php
+++ compraVenta/resources/views/index.blade.php
@@ -6,27 +6,25 @@
 <div class="container">
     <div class="row">
         @foreach ($sales as $sale)
-            @if (!$sale->issold)
-                <div class="col-md-4">
-                    <div class="card mb-4">
-                        <img src="{{ asset('storage/' . $sale->image) }}" class="card-img-top" alt="{{ $sale->product }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $sale->product }}</h5>
-                            <p class="card-text">{{ $sale->description }}</p>
-                            <p class="card-text">${{ number_format($sale->price, 2) }}</p>
-                            <form action="{{ route('sales.buy', $sale) }}" method="POST">
-                                @csrf
-                                <button type="submit" class="btn btn-primary">Comprar</button>
-                            </form>
-                        </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="{{ asset('storage/' . $sale->image) }}" class="card-img-top" alt="{{ $sale->product }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $sale->product }}</h5>
+                        <p class="card-text">{{ $sale->description }}</p>
+                        <p class="card-text">${{ number_format($sale->price, 2) }}</p>
+                        <form action="{{ route('sales.buy', $sale) }}" method="POST">
+                            @csrf
+                            <button type="submit" class="btn btn-primary">Comprar</button>
+                        </form>
                     </div>
                 </div>
-            @endif
+            </div>
         @endforeach
     </div>
 </div>
 @if(Auth::user())
-    <a href="{{ route('createnewproduct') }}">Crear un nuevo producto a la venta</a>
+    <button href="{{ route('createnewproduct') }}" class="btn btn-primary">Crear un nuevo producto</button>
 @else
     <a href="{{ route('login') }}">Iniciar sesión</a><br>
     <a href="{{ route('register') }}">Registrarse</a>
