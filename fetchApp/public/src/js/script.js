6634a34e80a4e6e58a17701b7ce44fba479d3240
Alejandro Melero Zhohal <brosgenepic@gmail.com>
2025-02-25 18:24:19 +0000

    cambios

--- fetchApp/public/src/js/script.js
+++
@@ -1,7 +0,0 @@
-import ModalEvents from './ModalEvents.js';
-
-const csrf = document.querySelector('meta[name="csrf-token"]')['content'];
-const mainUrl = document.querySelector('meta[name="url-base"]')['content'];
-
-const modalEvents = new ModalEvents(mainUrl, csrf);
-modalEvents.init();
